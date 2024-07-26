ca7717757869498ed7802db6fa5812a4a053ef5f
Eyal Kalish <99074066+TEFalcon@users.noreply.github.com>
2024-07-26 23:58:28 +0300

    fully functional proj No2

--- No2_CountDown/No2.js
+++ No2_CountDown/No2.js
@@ -1,33 +1,48 @@
-const timeTxts = document.querySelectorAll("[id$='TimeTxt']");
 const DATE_LOCS_KEY = "CD_Key";
-console.log(timeTxts);
+const timeTxts = document.querySelectorAll("[id$='TimeTxt']");
 const goalDateElm = document.getElementById("goalDate");
 const goalTimeElm = document.getElementById("goalTime");
 
+let timer;
+let retArray;
 disablePastDates(goalDateElm);
+updateLocalStorage();
 
-function goToTimer() {
-    //checking if date is valid
-    if(compareDates() === 1){
-        // if(isNaN(goalTimeElm.value))
-        //     disablePastDates(goalTimeElm);
+
+let timerWorking = !IsTimerArrayEmpty();
+goToTimer(false); 
+
+
+function goToTimer(checkTime = true) {//bool
+    if(!timerWorking){
+        if(!checkTime){
+            return;    
+        }
         
-        setupTimer();
+            //checking if date is valid
+            if (compareDates() !== 1) {
+                //not valid
+                alert("please choose a valid time");
+                return;
+            }
+            else{
+                var ToDate = goalDateElm.value.split("-");
+                var ToTime = goalTimeElm.value;
+                WritingRetArray(ToDate + " " + ToTime);
+            }
+    }
+    
+    setupTimer();
     
     
-        toggleClassList("setUpTimer", 'active', 0);
-        toggleClassList("container", 'active', 1);
-    }
-    else{//not valid
-        alert("please choose a valid time");
-    }
+    toggleClassList("setUpTimer", 'active', 0);
+    toggleClassList("container", 'active', 1);
 }
 function goToSelection() {
-    resetTimer();
-    
-    
     toggleClassList("setUpTimer", 'active', 1);
     toggleClassList("container", 'active', 0);
+    resetTimer();
+    
 }
 
 function disablePastDates(object) {
@@ -37,10 +52,10 @@ function disablePastDates(object) {
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
     if (object.id === "goalDate") {
-
+        
         setMin = yyyy + '-' + mm + '-' + dd;
         goalDateElm.value = setMin;
-        goalDateElm.setAttribute("Min",setMin);
+        goalDateElm.setAttribute("Min", setMin);
     }
     else if (object.id === "goalTime") {
         var HH = String(today.getHours()).padStart(2, '0');
@@ -48,55 +63,80 @@ function disablePastDates(object) {
         if (goalDateElm.value === (yyyy + '-' + mm + '-' + dd)) {
             setMin = HH + ":" + MM;
             goalTimeElm.value = setMin;
-
+            
             console.log((new Date().getTime() - new Date(goalDateElm.value).getTime()) / 1000 / 60 / 60);
         }
     }
 }
 
 function setupTimer() {
-    var today = new Date();
-    var ToDate = goalDateElm.value.split("-");
-    var ToTime = goalTimeElm.value;
-    var calcGoal = (new Date(ToDate + " " + ToTime));
-    console.log(calcGoal);
-    let yearDiff = (calcGoal.getFullYear() -  today.getFullYear());
-    if(yearDiff === 0){
-        
-    }
+    displayTime(retArray[0], retArray[1], retArray[2], retArray[3]);
     
-    
-    
-    // timeTxts[0] = 
+    timer = setInterval(function (){
+        if(retArray[3] === 0){
+            if(retArray[2] === 0){
+                if(retArray[1] === 0){
+                    if(retArray[0] === 0){
+                        resetTimer();
+                        alert("Countdown Finished!");
+                        return;
+                    }
+                    else{
+                        retArray[0] -= 1;
+                        retArray[1] = 23;
+                        retArray[2] = 59;
+                        retArray[3] = 60;
+                    }
+                }
+                else{
+                    retArray[1] -= 1;
+                    retArray[2] = 59;
+                        retArray[3] = 60;
+                    }
+                }
+                else{
+                    retArray[2] -= 1;
+                    retArray[3] = 60;
+            }
+        }
+        retArray[3] -= 1;
+        updateLocalStorage();
+        console.log(retArray[3]);
+    },1000);
+
+    timerWorking = true;
 }
 
-function compareDates(){
+function compareDates() {
     //returns 1 if the date is valid
     var today = new Date();
-    today.setMinutes(today.getMinutes() -1,59);
+    today.setMinutes(today.getMinutes() - 1, 59);
     var ToDate = goalDateElm.value;
     var ToTime = goalTimeElm.value;
     var calcGoal = (new Date(ToDate + " " + ToTime));
 
-if (today <  calcGoal) {
-  return 1;
-} else if (today >  calcGoal) {
-    return 0;
-} else {
-    console.log("today and date2 are the same");//then check hour
-  if(ToTime[0] > today.getHours()){
-    return 1;
-  }
-  else if(ToTime[0] === today.getHours() && ToTime[1] > today.getMinutes()){
-    return 1;
-  }
-  return 0;
+    if (today < calcGoal) {
+        return 1;
+    } else if (today > calcGoal) {
+        return 0;
+    } else {
+        console.log("today and date2 are the same");//then check hour
+        if (ToTime[0] > today.getHours()) {
+            return 1;
+        }
+        else if (ToTime[0] === today.getHours() && ToTime[1] > today.getMinutes()) {
+            return 1;
+        }
+        return 0;
+    }
 }
-}
-
 
 function resetTimer() {
+    updateLocalStorage(1);
 
+    timerWorking = false;
+    clearInterval(timer);
+    timer = null;
 }
 
 function toggleClassList(id, str, mode) {
@@ -113,12 +153,61 @@ function toggleClassList(id, str, mode) {
     }
 }
 
-
-function updateLocalStorage(){
+function displayTime(dd = 0, hh = 0, mm = 0, ss = 1) {
+    timeTxts[0].innerHTML = dd;
+    timeTxts[1].innerHTML = hh;
+    timeTxts[2].innerHTML = mm;
+    timeTxts[3].innerHTML = ss;
+}
+function updateLocalStorage(mode=0) {
+    //default, 0 - update during countdown
+    //1 - reset
     
+    
+    if(retArray == null){
+        if (localStorage.getItem(DATE_LOCS_KEY) == null || localStorage.getItem(DATE_LOCS_KEY).length===0) {
+            retArray = [0,0,0,0];//days,hours,minutes,seconds
+            localStorage.setItem(DATE_LOCS_KEY, JSON.stringify(retArray));
+        }
+        else {
+            retArray = JSON.parse(localStorage.getItem(DATE_LOCS_KEY));
+            console.log(retArray);
+        }    
+        return;
+    }
+    if(mode === 1){
+        retArray = [0,0,0,0];
+    }
+    localStorage.setItem(DATE_LOCS_KEY, JSON.stringify(retArray));
+    displayTime(retArray[0], retArray[1], retArray[2], retArray[3]);
+}
+
+function IsTimerArrayEmpty(){
+    //checiking if local storage is empty
+    for (let index = 0; index < retArray.length; index++) {
+        if (retArray[index] !== 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
-function CheckLocalStorage(){
+function WritingRetArray(calcGoalStr){
+    let calcGoal = (new Date(calcGoalStr));
+    console.log(calcGoal);
+    var today = new Date();
+    
+    let Difference_In_Time = calcGoal.getTime() - today.getTime();
+    
+    let Difference_In_Days = Math.floor(Difference_In_Time / (1000 * 3600 * 24));
+    Difference_In_Time = Difference_In_Time - (Difference_In_Days * 1000 * 3600 * 24);
+    let Difference_In_Hours = Math.floor((Difference_In_Time) / (1000 * 60 * 60 ));
+    Difference_In_Time = Difference_In_Time - (Difference_In_Hours * 1000 * 60 * 60);
+    let Difference_In_Minutes = Math.floor((Difference_In_Time) / (1000 * 60 ));
+    Difference_In_Time = Difference_In_Time - (Difference_In_Minutes * 1000 * 60);
+    let Difference_In_Seconds = Math.floor((Difference_In_Time) / 1000);
+    
 
+    retArray = [Difference_In_Days,Difference_In_Hours,Difference_In_Minutes,Difference_In_Seconds];
 }
